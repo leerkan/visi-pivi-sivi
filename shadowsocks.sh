785485927d5441df545b68fe6e936ab44e7ce045
leerkan <leerkan@outlook.com>
2018-02-08 15:44:59 -0500

    install shadowsocks

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -4,5 +4,8 @@ LC_ALL=C
 # BEGIN Tool Functions
 #. $(dirname $0)/pffs
 # END Tool Functions
+#
+# install with python2: 
+# pip install shadowsocks
 
 ssserver -c /usr/local/bin/shadowsocket.json -d start
